defcfbb2f8e2dae7b98e62f74876523ac1f12578
Kian Ebtekari <kianebtekari0@gmail.com>
2025-06-10 00:19:56 +0330

    [fix]: initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+        <ul>
+            <a href="#">HOME <span className=''>.</span></a>
+            <a href="#">PROJECTS.</a>
+            <a href="#">ABOUT US.</a>
+            <a href="#">CONTACT US.</a>
+        </ul>
+    </header>
+  )
+}
